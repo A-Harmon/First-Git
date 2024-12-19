7d7b7098950940c4788411b6d87d5d460ecb90a1
Anastasia Harmon <ahh2018@icloud.com>
2024-12-19 09:14:56 -0500

    Initial Commit

---
+++ First Git/First_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_GitApp.swift
+//  First Git
+//
+//  Created by Anastasia Harmon on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct First_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ First GitUITests/First_GitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  First_GitUITestsLaunchTests.swift
+//  First GitUITests
+//
+//  Created by Anastasia Harmon on 12/19/24.
+//
+
+import XCTest
+
+final class First_GitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
